ea1a88ca15bb3fa237a8deb427047d12736a8659
Karl Horky <karl.horky@gmail.com>
2023-03-26 18:39:33 +0200

    Switch manifest config to TypeScript

---
+++ manifest.config.ts
@@ -0,0 +1,18 @@
+import { defineManifest } from '@crxjs/vite-plugin';
+import packageJson from './package.json';
+
+// Convert from Semver (example: 0.1.0-beta6)
+const [major, minor, patch, label = '0'] = packageJson.version
+  // can only contain digits, dots, or dash
+  .replace(/[^\d.-]+/g, '')
+  // split into version parts
+  .split(/[.-]/);
+
+export default defineManifest(async (env) => ({
+  manifest_version: 3,
+  name: 'CRXJS React Vite Example',
+  // up to four numbers separated by dots
+  version: `${major}.${minor}.${patch}.${label}`,
+  // semver is OK in "version_name"
+  version_name: packageJson.version,
+}));
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { crx } from '@crxjs/vite-plugin';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import manifest from './manifest.json';
+import manifest from './manifest.config.js';
 
 export default defineConfig({
   plugins: [react(), crx({ manifest })],
